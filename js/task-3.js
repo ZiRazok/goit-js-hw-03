ea089ebb2744d44e881ad9878c0f5d7dcb9e9d35
ZiRazok <45107612+ZiRazok@users.noreply.github.com>
2024-03-22 21:53:48 +0200

    HW-JS-03
    Homework done

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,10 @@
-function checkForSpam(message) {
-    return message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale');
+function filterArray(numbers, value) {
+    let arr = [];
+    for (const i of numbers) if (i > value) arr.push(i);
+    return arr;
 }
-console.log(checkForSpam("Latest technology news"));
-console.log(checkForSpam("JavaScript weekly newsletter"));
-console.log(checkForSpam("Get best sale offers now!"));
-console.log(checkForSpam("Amazing SalE, only tonight!"));
-console.log(checkForSpam("Trust me, this is not a spam message"));
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
-console.log(checkForSpam("[SPAM] How to earn fast money?"));
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
